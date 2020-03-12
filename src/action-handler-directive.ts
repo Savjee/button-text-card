21be16ae0b1557deb572f38a7e76a64c796b72e7
Xavier Decuyper <xavier.decuyper@gmail.com>
2020-03-12 15:40:53 +0100

    Update action-handler-directive with unique ID for the card

--- src/action-handler-directive.ts
+++ src/action-handler-directive.ts
@@ -165,15 +165,15 @@ class ActionHandler extends HTMLElement implements ActionHandler {
 }
 
 // TODO You need to replace all instances of "action-handler-boilerplate" with "action-handler-<your card name>"
-customElements.define('action-handler-boilerplate', ActionHandler);
+customElements.define('action-handler-button-text-card', ActionHandler);
 
 const getActionHandler = (): ActionHandler => {
   const body = document.body;
-  if (body.querySelector('action-handler-boilerplate')) {
-    return body.querySelector('action-handler-boilerplate') as ActionHandler;
+  if (body.querySelector('action-handler-button-text-card')) {
+    return body.querySelector('action-handler-button-text-card') as ActionHandler;
   }
 
-  const actionhandler = document.createElement('action-handler-boilerplate');
+  const actionhandler = document.createElement('action-handler-button-text-card');
   body.appendChild(actionhandler);
 
   return actionhandler as ActionHandler;
